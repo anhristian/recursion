bcb32d9078414e33aa9bbad352ef4fe786c98a56
Anastasia Hristian <an.hristian@gmail.com>
2020-06-10 08:47:14 -0600

    Used the testDenormalized method and tested the Palindromes.

--- src/edu/cnm/deepdive/Palindromes.java
+++ src/edu/cnm/deepdive/Palindromes.java
@@ -3,22 +3,26 @@ package edu.cnm.deepdive;
 public class Palindromes {
 
 public static boolean testRecursive(String input) {
-//  boolean isPalindrom = false;
-//  if(
-//      input.length() <= 1
-//  || (
-//      input.charAt(0) == input.charAt(input.length()-1)
-//          && testRecursive(input.substring(1, input.length()-1))
-//          )
-//  ){
-//    isPalindrom = true;
-//  }
-// return false;
-  return input.length() <= 1
-      || (
-      input.charAt(0) == input.charAt(input.length() - 1)
-          && testRecursive(input.substring(1, input.length() - 1))
-  );
+  boolean isPalindrom = false;
+ if(
+      input.length() <= 1
+   || (
+     input.charAt(0) == input.charAt(input.length()-1)
+       && testRecursive(input.substring(1, input.length()-1))
+          ))
+ {
+    isPalindrom = true;
+ }
+ return isPalindrom;
 
+//  return input.length() <= 1
+//      || (
+  //    input.charAt(0) == input.charAt(input.length() - 1)
+ //         && testRecursive(input.substring(1, input.length() - 1))
+ //);
+  }
+  public static boolean testDenormalized(String input) {
+  String normalized = input.replaceAll("[\\W_]+","").toLowerCase();
+  return testRecursive(normalized);
   }
 }
--- test/edu/cnm/deepdive/PalindromesTest.java
+++ test/edu/cnm/deepdive/PalindromesTest.java
@@ -13,8 +13,18 @@ static final String[] knownPalindromes = {
  };
 static final String[] knownNonPalindromes = {
     "sonar",
-    "abb",
+    "abb"
 };
+static final String[] knownDenormalized = {
+    "radar",
+    "Radar",
+    "A man, a plan, a canal - Panama!",
+    "aBbA"
+};
+static final String[] knownNonDenormalized = {
+    "A man, a plan, a dam - Hoover!",
+};
+
   @Test
   void testRecursive() {
     assertTrue(Palindromes.testRecursive("radar"));
@@ -23,5 +33,16 @@ static final String[] knownNonPalindromes = {
     assertTrue(Palindromes.testRecursive(""));
     assertFalse(Palindromes.testRecursive("sonar"));
     assertFalse(Palindromes.testRecursive("abb"));
+
+  }
+
+  @Test
+  void testDenormalized() {
+    assertTrue(Palindromes.testDenormalized("radar"));
+    assertTrue(Palindromes.testDenormalized("Radar"));
+    assertTrue(Palindromes.testDenormalized("A man, a plan, a canal - Panama!"));
+    assertTrue(Palindromes.testDenormalized("aBba"));
+    assertFalse(Palindromes.testDenormalized("A man, a plan, a dam - Hoover"));
+
   }
 }
